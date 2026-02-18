f5dcb3ea8eb172ba4b06e39aa5460ae814a68f23
OnliestWizard <wizinjaconstruction@gmail.com>
2026-02-18 19:22:13 -0400

    Create emitJS.ts

---
+++ emitters/web/emitJS.ts
@@ -0,0 +1,14 @@
+import { ExecutionNode } from '../../node/NodeSchema'
+
+export function emitJS(node: ExecutionNode): string {
+  if (!node.sideEffects.includes('microphone')) {
+    throw new Error('Unsupported node for Web emitter')
+  }
+
+  return `
+export async function ${node.id}() {
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+  return await recordAudio(stream)
+}
+`
+}
